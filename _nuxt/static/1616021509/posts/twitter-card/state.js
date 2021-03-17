873a4c0d830a1fef1fd5d31ca81bbb87a75294df
elirehema <29277048+elirehema@users.noreply.github.com>
2021-03-17 22:52:08 +0000

    Deploying to gh-pages from  @ 02d8127c7653ea3adbedc88ab2666a6466177599 🚀

--- _nuxt/static/1616021509/posts/twitter-card/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_content\u002F_nuxt\u002Fstatic\u002F1616021509",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Ftwitter-card",config:{app:{basePath:"\u002Fnuxt_content\u002F",assetsPath:"\u002Fnuxt_content\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b4b50566"}}}}(null));
