e502e1606d072076f19b628847fcd3882ab5db99
elirehema <29277048+elirehema@users.noreply.github.com>
2021-04-12 17:52:23 +0000

    Deploying to gh-pages from  @ cdbe5c561085b579a0db8886426263ece1becff2 🚀

---
+++ _nuxt/static/1618249931/tags/climate/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_content\u002F_nuxt\u002Fstatic\u002F1618249931",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Fclimate",config:{_app:{basePath:"\u002Fnuxt_content\u002F",assetsPath:"\u002Fnuxt_content\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5f42aa31"}}}}(null));
