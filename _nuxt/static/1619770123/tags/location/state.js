50d978335dcfb6e5c5947e685695f1898c9ff893
elirehema <29277048+elirehema@users.noreply.github.com>
2021-04-30 08:09:05 +0000

    Deploying to gh-pages from  @ 03203fc0a8a6882ad8c298cc43c7da07957d40f6 🚀

--- _nuxt/static/1619770123/tags/location/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_content\u002F_nuxt\u002Fstatic\u002F1619770123",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Flocation",config:{_app:{basePath:"\u002Fnuxt_content\u002F",assetsPath:"\u002Fnuxt_content\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"96fce169"}}}}(null));
