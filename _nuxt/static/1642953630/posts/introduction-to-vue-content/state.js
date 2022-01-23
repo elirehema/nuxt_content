80420f209831db0a8eac397349d6cee64cd1c9e3
elirehema <29277048+elirehema@users.noreply.github.com>
2022-01-23 16:00:43 +0000

    Deploying to gh-pages from  @ b7189f7459abc6c18b0dfc1b045c2f33a5e27ef8 🚀

--- _nuxt/static/1642953630/posts/introduction-to-vue-content/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_content\u002F_nuxt\u002Fstatic\u002F1642953630",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Fintroduction-to-vue-content",config:{_app:{basePath:"\u002Fnuxt_content\u002F",assetsPath:"\u002Fnuxt_content\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c30d8c23"}}}}(null));
