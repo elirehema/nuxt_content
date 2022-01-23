c932c0db50a3161e03f2398998a3d3d3d6a6504c
elirehema <29277048+elirehema@users.noreply.github.com>
2022-01-23 15:38:32 +0000

    Deploying to gh-pages from  @ 299536d2b033b5edfdb8ad1ec80aad6b629ebd4d 🚀

--- _nuxt/static/1619770195/tags/vue/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_content\u002F_nuxt\u002Fstatic\u002F1619770195",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Fvue",config:{_app:{basePath:"\u002Fnuxt_content\u002F",assetsPath:"\u002Fnuxt_content\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8821bf12"}}}}(null));
