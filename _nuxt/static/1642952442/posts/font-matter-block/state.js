a3721060fb60f74238046c746cb75caf60c38fd2
elirehema <29277048+elirehema@users.noreply.github.com>
2022-01-23 16:00:13 +0000

    Deploying to gh-pages from  @ a575a2cf7384c1bdbf9e70e31490ccf412bd9fae 🚀

--- _nuxt/static/1642952442/posts/font-matter-block/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_content\u002F_nuxt\u002Fstatic\u002F1642952442",layout:"default",error:a,serverRendered:true,routePath:"\u002Fposts\u002Ffont-matter-block",config:{_app:{basePath:"\u002Fnuxt_content\u002F",assetsPath:"\u002Fnuxt_content\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7c352b76"}}}}(null));
---
+++ _nuxt/static/1642953607/tags/vue/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_content\u002F_nuxt\u002Fstatic\u002F1642953607",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Fvue",config:{_app:{basePath:"\u002Fnuxt_content\u002F",assetsPath:"\u002Fnuxt_content\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b90b36f2"}}}}(null));
